69c66b53a9937a94ff74afc46b55c839116ce07c
Vadim <magnalov@mail.ru>
2021-10-10 22:13:27 +0300

    some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { ReactElement } from "react";
+import FavouritesPage from "./Pages/FavouritesPage";
+import MainPage from "./Pages/MainPage";
+import PurchasesPage from "./Pages/PurchasesPage";
+import AppWrapper from "./StyledComponents/AppWrapper";
 
-function App() {
+// https://61606764faa03600179fba4e.mockapi.io/sneakers
+
+function App(): ReactElement {
   return (
-    <div className="App">
-      
-    </div>
+    <AppWrapper>
+      <MainPage />
+      <PurchasesPage />
+      <FavouritesPage />
+    </AppWrapper>
   );
 }
 
